ecaafc659472cc359d432d173d10cb4dd9e9014f
amar-basria <54560999+amar-basria@users.noreply.github.com>
2019-09-04 17:45:34 -0400

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Pause to display Hello World
+            Console.WriteLine("Press any key to continue....");
+
+            //Display Hello World
+            Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
+        }
+    }
+}
