4a9a5349c2481bf3c99436f6041e64d9bb7dbf4b
amar-basria <54560999+amar-basria@users.noreply.github.com>
2019-09-10 17:10:07 -0400

    Add files via upload

---
+++ Iterative_Statement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Iterative_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			try
+			{
+
+
+
+			Console.WriteLine("Please enter an integar value between 1 and 100: ");
+			int integar = int.Parse(Console.ReadLine());
+			   for(int x=1; x<11; x++)
+
+			  {
+			Console.WriteLine("You have entered " + integar + " This is the current integer value in the loop: " + x.ToString());
+			  }
+
+
+
+			 }
+			catch
+			{
+			Console.WriteLine("Please enter a integar");
+			}
+		}
+	}
+}
