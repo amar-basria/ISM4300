f1ed2e448477973e166a7664a1b4db59535bb5ab
amar-basria <54560999+amar-basria@users.noreply.github.com>
2019-09-11 16:08:36 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Iterative_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                for (int i = 0; i < 10; i++)
+                {
+
+                    Console.WriteLine("Please enter an integar value between 1 and 100: ");
+
+                    int integar = int.Parse(Console.ReadLine());
+
+
+                    if (integar < 0 || integar > 100)
+                    {
+                        Console.WriteLine("Integer is not within the range of 1-100");
+
+                    }
+                    else
+                    {
+                        for (int x = 1; x < 11; x++)
+
+                        {
+                            Console.WriteLine("You have entered " + integar + " This is the current integer value in the loop: " + x.ToString());
+                        }
+
+
+
+                    }
+
+                }
+            }
+            catch
+            {
+
+
+
+                Console.WriteLine("Please enter a integar");
+
+            }
+		}
+	}
+}
