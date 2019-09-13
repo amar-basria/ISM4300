91736a28d8fe2e0c1db6bc5b0256ce8bd9af60a8
amar-basria <54560999+amar-basria@users.noreply.github.com>
2019-09-12 22:05:17 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace Iterative_Statement
+namespace Conditional_Statement
 {
     class Program
     {
@@ -8,42 +8,89 @@ namespace Iterative_Statement
         {
             try
             {
-
-                for (int i = 0; i < 10; i++)
+                for (int x = 0; x < 10; x++)
                 {
-
-                    Console.WriteLine("Please enter an integer value between 1 and 100: ");
-
-                    int integer = int.Parse(Console.ReadLine());
-
-
-                    if (integer < 0 || integer > 100)
+                    Console.WriteLine("Enter your marks for ISM 4300 ");
+                    int marks = int.Parse(Console.ReadLine());
+                    if (marks < 0 || marks > 100)
                     {
-                        Console.WriteLine("Integer is not within the range of 1-100");
+                        Console.WriteLine("Invalid Number");
+                    }
+                    else if (marks < 60)
+                    {
+                        Console.WriteLine("Your grade for this class is F");
 
                     }
-                    else
+                    else if (marks >= 60 && marks <= 61)
                     {
-                        for (int x = 1; x < 11; x++)
-
-                        {
-                            Console.WriteLine("You have entered " + integer + " This is the current integer value in the loop: " + x.ToString());
-                        }
-
-
+                        Console.WriteLine("Your grade for this class is D-");
 
                     }
+                    else if (marks >= 62 && marks <= 67)
+                    {
+                        Console.WriteLine("Your grade for this class is D");
+
+                    }
+                    else if (marks >= 68 && marks <= 69)
+                    {
+                        Console.WriteLine("Your grade for this class is D+");
+
+                    }
+                    else if (marks >= 70 && marks <= 71)
+                    {
+                        Console.WriteLine("Your grade for this class is C-");
+
+                    }
+                    else if (marks >= 72 && marks <= 77)
+                    {
+                        Console.WriteLine("Your grade for this class is C");
+
+                    }
+                    else if (marks >= 78 && marks <= 79)
+                    {
+                        Console.WriteLine("Your grade for this class is C+");
+
+                    }
+                    else if (marks >= 80 && marks <= 81)
+                    {
+                        Console.WriteLine("Your grade for this class is B-");
+
+                    }
+                    else if (marks >= 82 && marks <= 87)
+                    {
+                        Console.WriteLine("Your grade for this class is B");
+
+                    }
+                    else if (marks >= 88 && marks <= 89)
+                    {
+                        Console.WriteLine("Your grade for this class is B+");
+
+                    }
+                    else if (marks >= 90 && marks <= 91)
+                    {
+                        Console.WriteLine("Your grade for this class is A-");
+
+                    }
+                    else if (marks >= 92 && marks <= 97)
+                    {
+                        Console.WriteLine("Your grade for this class is A");
+
+                    }
+                    else if (marks >= 98 && marks <= 100)
+                    {
+                        Console.WriteLine("Your grade for this class is A+");
+
+                    }
+
 
                 }
+                
             }
             catch
             {
-
-
-
-                Console.WriteLine("Please enter a integer");
-
+                Console.WriteLine("Please enter an integer");
             }
-		}
-	}
+
+        }
+    }
 }
