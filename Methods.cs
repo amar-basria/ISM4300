5b8fd28850e09c29d0a494e58046a59adc8123f4
amar-basria <54560999+amar-basria@users.noreply.github.com>
2019-09-19 21:05:13 -0400

    Add files via upload

---
+++ Methods.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                GetName();
+
+
+            }
+
+                void GetName()
+                {
+
+                    Console.WriteLine("Please enter your name: ");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("Hello " + name + "!");
+                }
+
+            
+
+        }
+    }
+}
